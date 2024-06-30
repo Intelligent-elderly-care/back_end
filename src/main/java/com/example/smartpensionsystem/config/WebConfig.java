bb669fe764e7f0ee556b40b03190a5c8aae342ca
李沛阳 <2199098121@qq.com>
2024-06-30 10:44:38 +0800

    Update WebConfig.java

--- src/main/java/com/example/smartpensionsystem/config/WebConfig.java
+++ src/main/java/com/example/smartpensionsystem/config/WebConfig.java
@@ -1,14 +1,15 @@
 package com.example.smartpensionsystem.config;
 
-
 import com.example.smartpensionsystem.interceptors.LoginInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
     @Autowired
     private LoginInterceptor loginInterceptor;
 
@@ -16,5 +17,14 @@ public class WebConfig implements WebMvcConfigurer {
     public void addInterceptors(InterceptorRegistry registry) {
         // 登录接口,注册接口，swagger不拦截,其它的都需要携带请求头
         registry.addInterceptor(loginInterceptor).excludePathPatterns("/register","/login","/swagger-ui/**", "/v3/api-docs/**");
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        // 配置跨域，允许任何域名的请求
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*");
     }
 }
